a65637a0e889d2682dac09e12f33ccdddd6dc450
Cristian <cristian.empanada@gmail.com>
2026-05-14 14:44:10 -0500

    Fix: noStore compat en Next 16

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { noStore } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 import { getStories, getLatestPendingToken } from "@/lib/turnos";
 
 export default async function Home() {
